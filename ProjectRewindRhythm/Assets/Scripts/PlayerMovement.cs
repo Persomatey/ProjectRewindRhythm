a0a7d5a07c7b587bdeffd4758e2e07b610d06aa8
Hunter Goodin <huntergoodin1@gmail.com>
2020-08-04 02:30:59 -0700

    CL-000002 - Started making Level 1
    Made the following changes: 
    - Made a new scene for Level 1 
    - Mapped out the whole level with collisions based on Super Mario Bros Level 1-1 
    - Started creating the player jumping/landing points throughout the level (only like 10% done right now though) 
    - Set it so that the player gets 999 misses in Level 1 (this is temporary and only for debugging purposes) 
    - Put Debug.Log() statements all over the place for easier debugging 
    - Added a .gitignore file to the repository 
    - Included a jumping SFX for the player in the SFX folder (unimplemented)

--- ProjectRewindRhythm/Assets/Scripts/PlayerMovement.cs
+++ ProjectRewindRhythm/Assets/Scripts/PlayerMovement.cs
@@ -5,37 +5,33 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    /* Locations
-     *   Obstacle   >     Jump 
-     * (15, 1.5, 0) > (17, 1.5, 0) 
-     * (0, 1.5, 0) > (2, 1.5, 0)
-     * (-10, 1.5, 0) > (-8, 1.5, 0)
-     * (#, #, #) > (#, #, #)
-     * (#, #, #) > (#, #, #)
-     */
-
     private Rigidbody playerRb;
+    public int jumpIndex;
     [Header("Jump Locations")]
     public Vector3[] jumpPoints;
     [Header("Jump Forces")]
     public int[] jumpForces; 
-    public int jumpIndex;
+    [Header("Speed Changes")]
+    public float[] speedChanges;
+    //[Header("Bool CAtch")]
+    //public bool[] boolCatch;
     [Header("Other Variables")]
-    public float speed;
-    private bool allowedToCheck;
+    public float curSpeed;
+    public bool allowedToCheck;
     private bool tripped;
     private bool pressedButton;
     public float generosity;
     public int strikes;
     public int strikesAllowed;
-    private bool allowedToJump; 
+    public bool allowedToJump; 
 
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         jumpIndex = 0;
         pressedButton = false;
-        allowedToJump = true; 
+        allowedToJump = true;
+        curSpeed = speedChanges[0];
     }
 
     void Update()
@@ -48,50 +44,66 @@ public class PlayerMovement : MonoBehaviour
 
         if (!tripped)
         {
-            transform.Translate(Vector3.left * Time.deltaTime * speed);
+            transform.Translate(Vector3.left * Time.deltaTime * curSpeed);
         }
 
-        if (jumpIndex < jumpPoints.Length && transform.position.x < jumpPoints[jumpIndex].x + 0.1f && allowedToJump)
+        if (jumpIndex < jumpPoints.Length && transform.position.x < jumpPoints[jumpIndex].x + 0.2f && allowedToJump)
         {
-            Debug.Log("Player Jumped because player is at " + transform.position); 
+            Debug.Log("<color=purple>Doing jump </color>" + jumpIndex + "<color=purple> because player is at </color>" + transform.position + "<color=purple> with </color>" + jumpForces[jumpIndex] + "<color=purple> force.</color>");
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
             playerRb.AddForce(Vector3.up * jumpForces[jumpIndex]);
             allowedToJump = false; 
             Invoke("AllowToCheck", 0.1f);
-            Debug.Log("AllowedToCheck called in 0.1f"); 
+            Debug.Log("<color=lightblue>AllowedToCheck called in 0.1f</color>"); 
         }
 
         if (allowedToCheck)
         { 
-            if (jumpIndex <= jumpPoints.Length && transform.position.y <= jumpPoints[jumpIndex].y + generosity && transform.position.y > jumpPoints[jumpIndex].y && !pressedButton)
+            if (jumpIndex <= jumpPoints.Length && transform.position.y <= jumpPoints[jumpIndex + 1].y + generosity && transform.position.y > jumpPoints[jumpIndex].y && !pressedButton)
             {
                 if ( Input.GetKeyDown(KeyCode.Space) )
                 {
                     Debug.Log("<color=green>Spacebar pressed! Can sucessfully land.</color>"); 
                     pressedButton = true;
+                    Debug.Log("<color=magenta> incrimenting jumpIndex from " + jumpIndex + " to " + (jumpIndex + 1) + "</color>"); 
                     jumpIndex++;
+                    curSpeed = speedChanges[jumpIndex];
                     allowedToJump = true;
-                    allowedToCheck = false; 
+                    allowedToCheck = false;
+                    Debug.Log("<color=darkblue>No longer allowed to check</color>");
                 }
             }
-            else if (transform.position.y == jumpPoints[jumpIndex].y && !pressedButton)
+            else if (transform.position.y <= jumpPoints[jumpIndex + 1].y && !pressedButton)
             {
                 Debug.Log("<color=orange>Spacebar not pressed. Player gets a strike.</color>");
                 strikes++;
-                allowedToCheck = false;
+                Debug.Log("<color=magenta> incrimenting jumpIndex from " + jumpIndex + " to " + (jumpIndex + 1) + "</color>");
                 jumpIndex++;
+                curSpeed = speedChanges[jumpIndex];
                 allowedToJump = true;
+                allowedToCheck = false;
+                Debug.Log("<color=darkblue>No longer allowed to check</color>");
             }
         }
 
         if (pressedButton && transform.position.y == jumpPoints[jumpIndex-1].y)
         {
+            Debug.Log("Making presedButton false again"); 
             pressedButton = false; 
         }
+
+
+        //if (boolCatch[jumpIndex] && (transform.position.x <= jumpPoints[jumpIndex].x + 0.1f || transform.position.x >= jumpPoints[jumpIndex].x - 0.1f)) 
+        //{
+        //    Debug.Log("Allowing Jump Anyways"); 
+        //    allowedToJump = true; 
+        //}
     }
 
     void AllowToCheck()
     {
-        Debug.Log("AllowedToCheck is true");
+        Debug.Log("<color=blue>AllowedToCheck is true</color>");
         allowedToCheck = true; 
     }
 }
