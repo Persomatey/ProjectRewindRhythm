2d309c7490e559b458c5862e3e4e1bdd46ee1f6f
Hunter Goodin <huntergoodin1@gmail.com>
2020-08-03 22:41:05 -0700

    CL-000001 - Player gets 3 chances to miss
    Made the following changes: 
    - The player now gets 3 chances to miss timing 
    - If the player misses the timing three times, the player "trips" and can no longer move 
    - Adjusted the parameters for landing to make it a little more dynamic if we wanted the player to be able to land on surfaces higher than the ground floor

--- ProjectRewindRhythm/Assets/Scripts/PlayerMovement.cs
+++ ProjectRewindRhythm/Assets/Scripts/PlayerMovement.cs
@@ -7,13 +7,11 @@ public class PlayerMovement : MonoBehaviour
 {
     /* Locations
      *   Obstacle   >     Jump 
-     * (15, 01, 00) > (17, 01, 00) 
-     * (00, 01, 00) > (02, 01, 00)
-     * (##, ##, ##) > (##, ##, ##)
-     * (##, ##, ##) > (##, ##, ##)
-     * (##, ##, ##) > (##, ##, ##)
-     * (##, ##, ##) > (##, ##, ##)
-     * (##, ##, ##) > (##, ##, ##)
+     * (15, 1.5, 0) > (17, 1.5, 0) 
+     * (0, 1.5, 0) > (2, 1.5, 0)
+     * (-10, 1.5, 0) > (-8, 1.5, 0)
+     * (#, #, #) > (#, #, #)
+     * (#, #, #) > (#, #, #)
      */
 
     private Rigidbody playerRb;
@@ -21,57 +19,79 @@ public class PlayerMovement : MonoBehaviour
     public Vector3[] jumpPoints;
     [Header("Jump Forces")]
     public int[] jumpForces; 
-    private int jumpIndex;
-    [Header("Speed")]
+    public int jumpIndex;
+    [Header("Other Variables")]
     public float speed;
-    public bool allowedToCheck;
-    public bool tripped;
-    public bool pressedButton; 
+    private bool allowedToCheck;
+    private bool tripped;
+    private bool pressedButton;
+    public float generosity;
+    public int strikes;
+    public int strikesAllowed;
+    private bool allowedToJump; 
 
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         jumpIndex = 0;
-        pressedButton = false; 
+        pressedButton = false;
+        allowedToJump = true; 
     }
 
     void Update()
     {
+        if (strikes >= strikesAllowed)
+        {
+            tripped = true;
+            Debug.Log("<color=red>3 strikes, you're out!</color>"); 
+        }
+
         if (!tripped)
         {
             transform.Translate(Vector3.left * Time.deltaTime * speed);
         }
 
-        if (jumpIndex < jumpPoints.Length && transform.position.x < jumpPoints[jumpIndex].x + 0.1f)
+        if (jumpIndex < jumpPoints.Length && transform.position.x < jumpPoints[jumpIndex].x + 0.1f && allowedToJump)
         {
             Debug.Log("Player Jumped because player is at " + transform.position); 
             playerRb.AddForce(Vector3.up * jumpForces[jumpIndex]);
-            jumpIndex++;
-            Invoke("AllowToCheck", 0.1f); 
+            allowedToJump = false; 
+            Invoke("AllowToCheck", 0.1f);
+            Debug.Log("AllowedToCheck called in 0.1f"); 
         }
 
         if (allowedToCheck)
         { 
-            if (transform.position.y < 2f && transform.position.y > 1.5f && !pressedButton)
+            if (jumpIndex <= jumpPoints.Length && transform.position.y <= jumpPoints[jumpIndex].y + generosity && transform.position.y > jumpPoints[jumpIndex].y && !pressedButton)
             {
-                Debug.Log("Listening for spacebar... "); 
-                
                 if ( Input.GetKeyDown(KeyCode.Space) )
                 {
-                    Debug.Log("<color=green>Spacebar pressed! Sucessfully landed.</color>"); 
-                    pressedButton = true; 
+                    Debug.Log("<color=green>Spacebar pressed! Can sucessfully land.</color>"); 
+                    pressedButton = true;
+                    jumpIndex++;
+                    allowedToJump = true;
+                    allowedToCheck = false; 
                 }
             }
-
-            if (transform.position.y == 1.5f && !pressedButton)
+            else if (transform.position.y == jumpPoints[jumpIndex].y && !pressedButton)
             {
-                tripped = true; 
+                Debug.Log("<color=orange>Spacebar not pressed. Player gets a strike.</color>");
+                strikes++;
+                allowedToCheck = false;
+                jumpIndex++;
+                allowedToJump = true;
             }
+        }
+
+        if (pressedButton && transform.position.y == jumpPoints[jumpIndex-1].y)
+        {
+            pressedButton = false; 
         }
     }
 
     void AllowToCheck()
     {
+        Debug.Log("AllowedToCheck is true");
         allowedToCheck = true; 
     }
 }
