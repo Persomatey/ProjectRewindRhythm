9ad818625dce2a9f5eb8e5c3519f302207012ba5
Hunter Goodin <huntergoodin1@gmail.com>
2020-08-03 20:55:58 -0700

    Added Project
    The project is now in the repository 
    - The camera follows the player 
    - The player constantly moves left 
    - As the player approaches certain coordinates, the player jumps 
    - As the player lands, they must press spacebar or the player "trips" and doesn't land correctly

---
+++ ProjectRewindRhythm/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    private Transform playerTrans;
+    private Vector3 offset; 
+
+    void Start()
+    {
+        playerTrans = GameObject.Find("Player").transform;
+        offset = new Vector3(0, 1.5f, -10); 
+    }
+
+
+    void Update()
+    {
+        transform.position = playerTrans.position + offset; 
+    }
+}
---
+++ ProjectRewindRhythm/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    /* Locations
+     *   Obstacle   >     Jump 
+     * (15, 01, 00) > (17, 01, 00) 
+     * (00, 01, 00) > (02, 01, 00)
+     * (##, ##, ##) > (##, ##, ##)
+     * (##, ##, ##) > (##, ##, ##)
+     * (##, ##, ##) > (##, ##, ##)
+     * (##, ##, ##) > (##, ##, ##)
+     * (##, ##, ##) > (##, ##, ##)
+     */
+
+    private Rigidbody playerRb;
+    [Header("Jump Locations")]
+    public Vector3[] jumpPoints;
+    [Header("Jump Forces")]
+    public int[] jumpForces; 
+    private int jumpIndex;
+    [Header("Speed")]
+    public float speed;
+    public bool allowedToCheck;
+    public bool tripped;
+    public bool pressedButton; 
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        jumpIndex = 0;
+        pressedButton = false; 
+    }
+
+    void Update()
+    {
+        if (!tripped)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if (jumpIndex < jumpPoints.Length && transform.position.x < jumpPoints[jumpIndex].x + 0.1f)
+        {
+            Debug.Log("Player Jumped because player is at " + transform.position); 
+            playerRb.AddForce(Vector3.up * jumpForces[jumpIndex]);
+            jumpIndex++;
+            Invoke("AllowToCheck", 0.1f); 
+        }
+
+        if (allowedToCheck)
+        { 
+            if (transform.position.y < 2f && transform.position.y > 1.5f && !pressedButton)
+            {
+                Debug.Log("Listening for spacebar... "); 
+                
+                if ( Input.GetKeyDown(KeyCode.Space) )
+                {
+                    Debug.Log("<color=green>Spacebar pressed! Sucessfully landed.</color>"); 
+                    pressedButton = true; 
+                }
+            }
+
+            if (transform.position.y == 1.5f && !pressedButton)
+            {
+                tripped = true; 
+            }
+        }
+    }
+
+    void AllowToCheck()
+    {
+        allowedToCheck = true; 
+    }
+}
