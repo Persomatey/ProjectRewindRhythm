e7aa184b5f79a648e9e5d46d4bdfb01d5b4db5c6
Gerardo Bonnet <gerardo.bonnet@gmail.com>
2020-08-07 17:43:13 -0700

    Added Glow Effect
    The glow effect will help show the player when to press the jump button. It automatically moves to the next landing position and tracks player movement.
    I've carefully chosen which files to push to this branch so that it won't mess up during merge hopefully.

---
+++ ProjectRewindRhythm/Assets/Scripts/GlowBallUpper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowBallUpper : MonoBehaviour
+{
+    private GameObject player;
+    public float spawnY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        spawnY = this.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, player.transform.position.y - 1, transform.position.z);
+
+        if (transform.position.y < spawnY)
+            transform.position = new Vector3(transform.position.x, spawnY, transform.position.z);
+
+    }
+}
