e0d9c99c1a63028c020ee1d61f92b0282bc95650
Hunter Goodin <huntergoodin1@gmail.com>
2020-08-07 22:11:21 -0700

    CL-000009
    Changed the following: 
    - Implemented Peter's dying animation 
    - Flipped the block sprites so that the perceived lighting is the same as the player sprite's 
    - Made the background blocks move diagonally per Gerardo's request 
    - Fixed all the jumping SFX timings for every jump 
    - Added animations to the background of the main menu 
    - The buttons on the main menu float up and down 
    - Added the tripping animation for when the player loses 
    - Added the running animation for when the player is moving but not jumping 
    - Changed the way that the landings are determined (again) so that now the landings will register when the player is on the ground regardless of when they pressed the button (this was causing issues before with the timing of the speed changes)
    - Added a glitchy effect when the player loses 
    - Leveled the audio so it isn't suuuper loud 
    - (Hopefully) fixed Gerardo's mistakes from earlier

--- ProjectRewindRhythm/Assets/Scripts/BackgroundMovement.cs
+++ ProjectRewindRhythm/Assets/Scripts/BackgroundMovement.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Diagnostics;
 
-public class BackgroundMovementHunter : MonoBehaviour
+public class BackgroundMovement : MonoBehaviour
 {
     private float speed;
-    public float speedMod;
-    public bool allowedToMove;
+    public float speedMod; 
+    public bool allowedToMove; 
 
     void Update()
     {
-        speed = (GameObject.Find("Player").GetComponent<PlayerMovement>().curSpeed / 35) * speedMod;
+        speed = (GameObject.Find("Player").GetComponent<PlayerMovement>().curSpeed / 35) * speedMod; 
 
         if (GameObject.Find("Player").GetComponent<PlayerMovement>().gameStarted && !GameObject.Find("Player").GetComponent<PlayerMovement>().gameWon && !GameObject.Find("Player").GetComponent<PlayerMovement>().tripped)
         {
@@ -19,14 +19,14 @@ public class BackgroundMovementHunter : MonoBehaviour
         }
         else
         {
-            allowedToMove = false;
+            allowedToMove = false; 
         }
 
         if (allowedToMove)
         {
             transform.Translate(-Vector3.left * Time.deltaTime * speed);
         }
-        else
+        else 
         {
             transform.Translate(-Vector3.left * Time.deltaTime * 0.05f);
         }
